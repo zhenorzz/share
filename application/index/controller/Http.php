f036e3d56f42b8c192a59019e4daf06beb009625
Zane <liz@uclbrt.com>
2018-01-01 20:23:06 +0800

    xml

--- application/index/controller/Http.php
+++ application/index/controller/Http.php
@@ -16,9 +16,26 @@ class Http extends Controller
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if (! empty($param['headerParam'])) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $param['headerParam']);
+
+        }
         if ($param['method'] === 'POST' && ! empty($param['bodyParam'])) {
             curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $param['bodyParam']);
+            switch ($param['contentType']) {
+                case 'application/x-www-form-urlencoded':
+                    $data = $param['bodyParam'];
+                    break;
+                case 'application/json':
+                    $data = json_encode($param['bodyParam']);
+                    break;
+                case 'application/xml':
+                    $data = $param['bodyParam'];
+                    break;
+                default:
+                    $data = $param['bodyParam'];
+            }
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         }
         $body = curl_exec($ch);
         $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
